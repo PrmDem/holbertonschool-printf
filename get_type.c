fba7eda67e034892c34d880136bc712f59e4c5b1
Priam <priamdmy@gmail.com>
2025-07-22 11:05:52 +0200

    coded get_type fnct

---
+++ get_type.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* get_type - checks specifier in given string
+* @format: string given as input
+* @arg: value to replace specifier
+*
+* Return: number of written chars
+* as per 'write' function
+*/
+
+int get_type(char *format, va_list arg)
+{
+	int index = 0;
+
+	template types[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"i", print_integer},
+		{"d", print_decimal},
+		{"%", print_percent},
+		{NULL, NULL}
+	};
+
+	while (types[index].str != NULL)
+	{
+		if (*format == *(types[index].str))
+			return (types[index].f);
+
+		index++;
+	}
+
+	return (NULL);
+}
