ee2b1e002e12430f9863b5bea34b7d09ac3d8e97
chermat mustapha <mchermat@outlook.fr>
2025-07-24 11:14:40 +0200

    add our test file

---
+++ test/main.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+int main(void)
+{
+	int len, len2;
+	char *str = NULL;
+
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+	_printf("The string is null : [%s]\n", str);
+	printf("The string is null : [%s]\n", str);
+	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
+	_printf("Len:[%d]\n", len);
+	printf("Len:[%d]\n", len2);
+	_printf("More percent : [%%%%%%%%%%]\n");
+	printf("More percent : [%%%%%%%%%%]\n");
+	_printf("Unknown:[%r]\n");
+	printf("Unknown:[%r]\n");
+	_printf("More unknow [%r%r%r]\n");
+	printf("More unknow [%r%r%r]\n");
+	_printf("More than max_int : %d\n", 2344231123456789123);
+	printf("More than max_int : %d\n", 2344231123456789123);
+	return (0);
+}
