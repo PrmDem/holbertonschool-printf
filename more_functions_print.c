fb1c89e249baac45de70179629b87bc1944cb731
chermat mustapha <mchermat@outlook.fr>
2025-07-25 16:27:28 +0200

    create print_octal and print_unsigned functions

--- more_functions_print.c
+++ more_functions_print.c
@@ -28,7 +28,7 @@ int print_hexa(va_list arg)
 		if (n % 16 < 10)
 			buffer[i] = (n % 16) + '0';
 		else
-			buffer[i] =((n % 16) % 10) + 'A';
+			buffer[i] = ((n % 16) % 10) + 'A';
 
 
 		n /= 16;
@@ -72,7 +72,7 @@ int print_hexa_low(va_list arg)
 		if (n % 16 < 10)
 			buffer[i] = (n % 16) + '0';
 		else
-			buffer[i] =((n % 16) % 10) + 'a';
+			buffer[i] = ((n % 16) % 10) + 'a';
 
 
 		n /= 16;
@@ -82,6 +82,70 @@ int print_hexa_low(va_list arg)
 	while (i--)
 		count += write(1, &buffer[i], 1);
 
+
+	return (count);
+}
+
+/**
+*print_octal - print an integer in octal
+*@arg: the integer
+*Return: write int
+*/
+
+int print_octal(va_list arg)
+{
+	long int n;
+	int i = 0, count = 0;
+	char buffer[12];
+
+	n = va_arg(arg, int);
+
+	if (n == 0)
+		count += write(1, "0", 1);
+
+	if (n < 0)
+	{
+		n = -n;
+		n = 4294967296 - n;
+	}
+
+	while (n > 0)
+	{
+		buffer[i] = (n % 8) + '0';
+		n /= 8;
+		i++;
+	}
+	while (i--)
+		count += write(1, &buffer[i], 1);
+
+	return (count);
+}
+
+/**
+*print_unsigned - print an integer in usigned integer
+*@arg: the integer
+*Return: write int
+*/
+
+int print_unsigned(va_list arg)
+{
+	unsigned int n;
+	int i = 0, count = 0;
+	char buffer[12];
+
+	n = va_arg(arg, int);
+
+	if (n == 0)
+		count += write(1, "0", 1);
+
+	while (n > 0)
+	{
+		buffer[i] = (n % 10) + '0';
+		n /= 10;
+		i++;
+	}
+	while (i--)
+		count += write(1, &buffer[i], 1);
 
 	return (count);
 }
